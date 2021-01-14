7766d62f9eb2eb506cdf7d8c33cc0078aba5883a
Miquel Parera <computer.music.neix@gmail.com>
2021-01-14 19:02:48 +0100

    Update dhPreload.js

--- dhPreload.js
+++ dhPreload.js
@@ -1,46 +1,51 @@
-
-
 let audio1,sample1;
 let image1,draw1;
+let print;
+
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  loadStrings("assets/dhPreload.txt",fileParser);
-  loadFont('assets/Philosopher-Bold.ttf', drawText);
-  
+  loadStrings("./assets/dhPreload.txt",fileParser);
+  loadFont('./assets/Philosopher-Bold.ttf', drawText);
+
 }
 
 
 
 function draw(){
-  
+
 }
 
 
 function fileParser(textfile){
     for(let i = 0; i < textfile.length; i++){
       let tekken = splitTokens(textfile[i], ' ');
-     
+
+      if(tekken[0] == "print"){
+        print = tekken[1];
+      }
+
       if(tekken[0] == "audio"){
         audio1 = math.randomInt(tekken[1],tekken[2]);
-        sample1 = loadSound('assets/'+str(audio1)+'.wav',soundReady, null, whileLoading);
+        sample1 = loadSound('./assets/'+str(audio1)+'.wav',soundReady, null, whileLoading);
         console.log(audio1);
       }
       if(tekken[0] == "image"){
         image1 = math.randomInt(tekken[1],tekken[2]);
-        draw1 = loadImage('assets/'+image1+'.png',
+        draw1 = loadImage('./assets/'+image1+'.png',
       imagesLoad);
     }
-  
+
 }
 }
 
 function whileLoading(total) {
-  print('loaded: ' + total);
+  console.log('loaded: ' + total);
 }
 
 function soundReady() {
-  print('sound is ready');
+  console.log('sound is ready');
+  userStartAudio();
   sample1.loop();
 }
 
@@ -50,7 +55,7 @@ function imagesLoad() {
 }
 
 function drawText(font) {
-  fill('#ED225D');
+  fill('#39b89c');
   textFont(font, 36);
-  text('p5*js', 10, 50);
+  text(print, 10, 50);
 }
